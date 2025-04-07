40f54ce1a250cd99149205261edcb4b1a81d7475
kimu805 <kimukimuok805@gmail.com>
2025-04-07 14:46:51 +0900

    Update calculator_spec.rb

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,17 +1,10 @@
-RSpec.describe "計算機" do 
-  describe "足し算" do
-    it "2足す2は4になる" do
-      expect(2 + 2).to eq(4)
-    end
+require_relative "../calculator.rb"
 
-    it "1足す4は5になる" do
-      expect(1 + 4).to eq(5)
-    end
-  end
-
-  describe "引き算" do
-    it "5引く3は2になる" do
-      expect(5 - 3).to eq(2)
+RSpec.describe Calculator do
+  describe "#divide" do
+    it "0で割るとエラーが発生する" do
+      calc = Calculator.new
+      expect{ calc.divide(4, 0) }.to raise_error(ArgumentError)
     end
   end
 end
