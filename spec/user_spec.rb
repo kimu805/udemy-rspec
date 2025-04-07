d33d9db468231f860a10bea3616bc7b1de26eabe
kimu805 <kimukimuok805@gmail.com>
2025-04-07 13:27:54 +0900

    Update user_spec.rb

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,28 +1,5 @@
 require_relative "../user.rb"
 
 RSpec.describe User do
-  describe "#activated?" do
-    it "新規ユーザーは有効化されていない" do
-      user = User.new
-      expect(user.activated?).not_to be true
-    end
-    it "メール認証後のユーザーは有効化される" do
-      user = User.new
-      user.verify_email
-      expect(user.activated?).to be true
-    end
-  end
-
-  describe "#profile" do
-    it "新規ユーザーはプロフィールが未設定" do
-      user = User.new
-      expect(user.profile).to be_nil
-    end
-
-    it "プロフィール設定後は設定した内容を返す" do
-      user = User.new
-      user.set_profile("よろしくお願いします。")
-      expect(user.profile).to eq "よろしくお願いします。"
-    end
-  end
+  
 end
