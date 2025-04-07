2ca1463a1f2bcd4d44cc595a8939a55d0f659b17
kimu805 <kimukimuok805@gmail.com>
2025-04-07 14:53:37 +0900

    Update calculator_spec.rb

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe Calculator do
       calc = Calculator.new
       expect{ calc.divide(4, 0) }.to raise_error(ArgumentError)
     end
+
+    it "0で割るとエラーメッセージが表示される" do
+      calc = Calculator.new
+      expect { calc.divide(4, 0).to raise_error(ArgumentError, "0で割ることはできません")}
+    end
+
+    it "通常の割り算は成功する" do
+      calc = Calculator.new
+      expect { calc.divide(10, 2) }.not_to raise_error
+    end
   end
 end
