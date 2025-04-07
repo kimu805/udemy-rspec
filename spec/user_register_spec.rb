eefb9a2106c491385c2f4ea5ea86b97a27090b5d
kimu805 <kimukimuok805@gmail.com>
2025-04-07 17:06:51 +0900

    Create user_register_spec.rb

---
+++ spec/user_register_spec.rb
@@ -0,0 +1,14 @@
+require_relative "../user_register"
+
+RSpec.describe UserRegister do
+  describe "#register" do
+    it "ウェルカムメールが送信される" do
+      email_service = double(EmailService)
+
+      expect(email_service).to receive(:send_welcome_mail).with("test@example.com")
+
+      user_register = UserRegister.new(email_service)
+      user_register.register("test@example.com")
+    end
+  end
+end
