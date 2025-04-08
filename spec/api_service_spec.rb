8110b920be7abafd400c78d3e947d613e704ec1d
kimu805 <kimukimuok805@gmail.com>
2025-04-08 18:01:10 +0900

    Update api_service_spec.rb

--- spec/api_service_spec.rb
+++ spec/api_service_spec.rb
@@ -2,9 +2,24 @@ require_relative "../api_service"
 
 RSpec.describe ApiService do
   describe "#fetch_photos" do
+    let(:photos_response) do
+      [
+        {
+          id: 1,
+          title: "写真１",
+          url: "https://example.com/photo1.jpg"
+        }
+      ].to_json
+    end
+
+    before do
+      allow(Net::HTTP).to receive(:get).and_return(photos_response)
+    end
+
     it "写真データを取得できる" do
       service = ApiService.new
       photos = service.fetch_photos
+
       expect(photos).to be_an(Array)
       expect(photos.first).to include("id", "title", "url")
     end
