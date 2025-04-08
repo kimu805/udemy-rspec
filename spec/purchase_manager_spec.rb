6ea1d81d0beb2031a9802acb8d8e467b2925a044
kimu805 <kimukimuok805@gmail.com>
2025-04-08 18:44:55 +0900

    Update purchase_manager_spec.rb

--- spec/purchase_manager_spec.rb
+++ spec/purchase_manager_spec.rb
@@ -2,26 +2,16 @@ require_relative "../purchase_manager"
 
 RSpec.describe PurchaseManager do
   describe "#purchase" do
+    let(:stock_service) { instance_double(StockService, fetch_stock_quantity: 50) }
+    let(:stock_quantity) { 50 }
     let(:quantity) { 30 }
     let(:product_id) { 123 }
-    let(:stock_quantity) { 50 }
-    let(:stock_service) { instance_double(
-      StockService,
-      fetch_stock_quantity: stock_quantity
-    )}
-
-    subject {PurchaseManager.new(stock_service).purchase(product_id, quantity)}
+    subject { PurchaseManager.new(stock_service).purchase(product_id, quantity) }
 
     context "在庫が十分にある場合" do
       it "購入に成功する" do
-        expect(subject).to be true
-      end
-    end
-
-    context "在庫が足りない場合" do
-      let(:stock_quantity) { 29 }
-      it "購入に失敗する" do
-        expect(subject).to be false
+        result = subject
+        expect(result).to be true
       end
     end
   end
